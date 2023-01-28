ec6d9b7374a4a390b8b3e22d983b51a922b82792
Sergey Vorobev <ch4rlythec4t@gmail.com>
2023-01-28 15:11:56 +0300

    task3 done

---
+++ task3/script3.js
@@ -0,0 +1,10 @@
+let c = 10
+let d = 2
+sum = 10 + 2
+diff = 10 - 2
+div = 10 / 2
+mult = 10 * 2
+console.log(` Сумма ${c} и ${d} равна ${sum}`)
+console.log(` Разность ${c} и ${d} равна ${diff}`)
+console.log(` Деление ${c} и ${d} равна ${div}`)
+console.log(` Умножение ${c} и ${d} равна ${mult}`)
